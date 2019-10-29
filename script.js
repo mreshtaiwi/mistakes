3c8411fb5cecb1458d4f62273097cd5380d07800
mreshtaiwi <mr.eshtaiwi@gmail.com>
2019-10-29 23:45:18 +0200

    add more mistakes

--- script.js
+++ script.js
@@ -10,3 +10,24 @@ for (var i = 0; i <10; i++) {
 for (var i = 0; i < 10; i++) {
 }
 document.write(i);
+
+function checkname(name) {
+  if (name ="shihab") {
+    document.right('weclome '+ name);
+  } else {
+  document.write('welcome ' + name);  
+}
+}
+checkname();
+
+var x = 0;
+var y = 'shihab';
+z = x * y;
+console.log(z);
+
+var n = 10;
+var m = '10';
+var w = n * m;
+if(w=== 100){
+  console.log('yes it is 100');
+}
